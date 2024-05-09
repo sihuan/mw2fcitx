9547595905d9f7e2426bc9d67b5a7dda56fc5d90
Outvi V <git@outv.im>
2024-05-09 23:08:36 +0800

    fix: mw2fcitx doesn't generate titles.txt w/o API calls

--- utils/moegirl_dict.py
+++ utils/moegirl_dict.py
@@ -3,7 +3,7 @@ from mw2fcitx.tweaks.moegirl import tweaks
 exports = {
     "source": {
         "file_path": [
-            "extras/20240509.txt",
+            "titles.txt",
             "extras/pcr.txt"
         ],
         "kwargs": {
